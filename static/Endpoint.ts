bffd4b076cedc10e48da4dbf7410b2e27ed7eb03
pascholda1 <dev@pascholda.one>
2025-03-28 10:28:14 +0100

    fixed endpoint generation, prevent invalid URL construction when using relativ urls

--- static/Endpoint.ts
+++ static/Endpoint.ts
@@ -1,7 +1,7 @@
 export type Method = 'GET' | 'POST'
 
 export default class Endpoint<RequestData = unknown, ResponseData = unknown> {
-  readonly url: URL;
+  readonly url: string;
   readonly method: Method;
 
   // @ts-expect-error - required for Type Inference
@@ -9,7 +9,7 @@ export default class Endpoint<RequestData = unknown, ResponseData = unknown> {
   // @ts-expect-error - required for Type Inference
   responseData: ResponseData;
 
-  constructor(path: URL, method: Method) {
+  constructor(path: string, method: Method) {
     this.url = path;
     this.method = method;
   }
